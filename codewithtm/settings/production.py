91d44e50402dcd9b8aeebb152daada90e7af0c52
unknown <tachiefab311@gmail.com>
2020-08-03 15:29:10 -0700

    Removed duplicate allowed host

--- codewithtm/settings/production.py
+++ codewithtm/settings/production.py
@@ -186,6 +186,10 @@ STATIC_URL = '/static/'
 #     #'/var/www/static/',
 # )
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-server', 'media-root')
 # # MEDIA_URL = '/media/'
