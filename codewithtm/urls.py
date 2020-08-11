1e62732a9a49e1e628b66535d03e88a1e04602ef
unknown <tachiefab311@gmail.com>
2020-08-11 10:58:50 -0700

    Changed permissions

--- codewithtm/urls.py
+++ codewithtm/urls.py
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="Test License"),
     ),
     public=True,
-    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
